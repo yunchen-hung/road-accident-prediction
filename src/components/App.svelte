cf5e660b20bd3833e8cd8ed8da1652b99f3802fd
yunchen-hung <123218334+yunchen-hung@users.noreply.github.com>
2024-03-17 08:37:44 -0700

    basic set up

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,11 +3,28 @@
 </script>
 
 <main>
-  <h1>Svelte template</h1>
+  <body>
+  <h1>Accident Severity Prediction</h1>
 
-  <p>Write your HTML here</p>
+  <p>
+    POV: you got into a road accident in the UK... How severe is your accident?
+  </p>
+  <p>Play around with the dropdown to find out!</p>
+  </body>
 </main>
 
 <style>
-  /* Write your CSS here */
+  body {
+    background-color:black;
+  }
+  h1 {
+    color: white;
+    text-align: center;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+  p {
+    color: white;
+    text-align: center;
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
 </style>
